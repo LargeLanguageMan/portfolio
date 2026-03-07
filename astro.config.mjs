46ce7bb71fb78bb2892f2cfcb73006cb8f46b30a
Kai <wesley.hucker@innocean.com.au>
2026-03-07 09:14:45 +0000

    Add GA4 gtag snippet (G-29H3KSNVGS) to site head

--- astro.config.mjs
+++ astro.config.mjs
@@ -21,6 +21,24 @@ export default defineConfig({
       social: [
         { icon: 'github', label: 'GitHub', href: 'https://github.com/LargeLanguageMan' },
       ],
+      head: [
+        {
+          tag: 'script',
+          attrs: {
+            async: true,
+            src: 'https://www.googletagmanager.com/gtag/js?id=G-29H3KSNVGS',
+          },
+        },
+        {
+          tag: 'script',
+          content: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-29H3KSNVGS');
+          `,
+        },
+      ],
       sidebar: [
         { label: 'About', link: '/' },
       ],
